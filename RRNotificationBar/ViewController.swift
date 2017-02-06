eb6e5e676e3abb33e112ab3218699159bc97f0bc
Rafat Touqir Rafsun <rafsun.ra@gmail.com>
2017-02-06 11:52:34 +0600

    Initial Commit

---
+++ RRNotificationBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RRNotificationBar
+//
+//  Created by RAFAT TOUQIR RAFSUN on 2/6/17.
+//  Copyright © 2017 RAFAT TOUQIR RAFSUN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
