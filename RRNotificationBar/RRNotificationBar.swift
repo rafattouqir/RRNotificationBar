c410e0df0876fbf059c6da716f61974d8cbd8e23
Rafat Touqir Rafsun <rafsun.ra@gmail.com>
2017-02-07 13:12:40 +0600

    notification bar added

---
+++ RRNotificationBar/RRNotificationBar.swift
@@ -0,0 +1,9 @@
+//
+//  RRNotificationBar.swift
+//  RRNotificationBar
+//
+//  Created by RAFAT TOUQIR RAFSUN on 2/6/17.
+//  Copyright © 2017 RAFAT TOUQIR RAFSUN. All rights reserved.
+//
+
+import Foundation
